2053ef56b9604899a31252c97e2313f320fb6d01
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-04 15:51:41 +0000

    WIP: add document type selection model schema and tests

---
+++ spec/models/document_type_selection_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "json"
+
+RSpec.describe DocumentTypeSelection do
+  let(:document_type_selections) { YAML.load_file(Rails.root.join("config/document_type_selections.yml")) }
+
+  describe "all configured document types selections are valid" do
+    it "should conform to the document type selection schema" do
+      document_type_selection_schema = JSON.parse(File.read("config/schemas/document_type_selection.json"))
+      document_type_selections.each do |document_type_selection|
+        validator = JSON::Validator.fully_validate(document_type_selection_schema, document_type_selection)
+        expect(validator).to(
+          be_empty,
+          "Validation for #{document_type_selection['id']} failed: \n\t#{validator.join("\n\t")}",
+        )
+      end
+    end
+  end
+end
