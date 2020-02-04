622241a65d7bf3e50ed50fa2ba258f3328325303
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-02-04 14:34:22 +0000

    Thoughts so far
    Reduced from 3 routes to 2 routes
    Decided to handle all routing in controller, but allow interactor to do three things:
      1) return a redirect URL if managed elsewhere
      2) return a refine_further URL if the document type needs further specificity
      3) create a document and return its URL if we're at the end of the journey

--- app/controllers/new_document_controller.rb
+++ app/controllers/new_document_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class NewDocumentController < ApplicationController
-  def choose_document_type
-    result = NewDocument::ChooseSupertypeInteractor.call(params: params, user: current_user)
-    issues, @supertype = result.to_h.values_at(:issues, :supertype)
+  def choose_document_type # GET
+    render :choose_document_type,
+            assigns: { issues: issues, id: id }
+  end
+
+  def choose_or_create_document_type # POST
+    result = NewDocument::DocumentTypeSelectionInteractor.call(params: params, user: current_user)
 
     if result.issues
       flash.now["requirements"] = { "items" => issues.items }
@@ -11,28 +15,12 @@ class NewDocumentController < ApplicationController
       render :choose_supertype,
              assigns: { issues: issues },
              status: :unprocessable_entity
-    elsif @supertype.managed_elsewhere
-      redirect_to @supertype.managed_elsewhere_url
-    end
-  end
-
-  def create
-    result = NewDocument::CreateInteractor.call(params: params, user: current_user)
-    issues, supertype, document_type, document = result.to_h.values_at(:issues,
-                                                                       :supertype,
-                                                                       :document_type,
-                                                                       :document)
-
-    if issues
-      flash.now["requirements"] = { "items" => issues.items }
-
-      render :choose_document_type,
-             assigns: { issues: issues, supertype: supertype },
-             status: :unprocessable_entity
+    elsif result.refine_further
+      redirect_to refine_further
     elsif result.managed_elsewhere
-      redirect_to document_type.managed_elsewhere_url
-    else
-      redirect_to content_path(document)
+      redirect_to managed_elsewhere
+    elsif result.created_document_path
+      redirect_to created_document_path
     end
   end
 end
