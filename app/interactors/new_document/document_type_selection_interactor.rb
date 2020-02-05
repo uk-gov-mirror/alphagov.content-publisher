a0aeb431ba90a678ec14b5453978db07633cca47
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-05 12:12:17 +0000

    Interactor checks document type has been selected

--- app/interactors/new_document/document_type_selection_interactor.rb
+++ app/interactors/new_document/document_type_selection_interactor.rb
@@ -7,10 +7,23 @@ class NewDocument::DocumentTypeSelectionInteractor < ApplicationInteractor
 
 
   def call
+    check_for_issues
     find_document_type_selection
   end
 
 private
+
+  def check_for_issues
+    return if params[:document_type_selection_id].present?
+
+    context.fail!(issues: document_type_issues)
+  end
+
+  def document_type_issues
+    Requirements::CheckerIssues.new([
+      Requirements::Issue.new(:document_type_selection_id, :not_selected),
+    ])
+  end
 
   def find_document_type_selection
     context.document_type_selection = DocumentTypeSelection.find(params[:document_type_selection_id])
--- spec/interactors/new_document/document_type_selection_interactor_spec.rb
+++ spec/interactors/new_document/document_type_selection_interactor_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe NewDocument::DocumentTypeSelectionInteractor do
       result = NewDocument::DocumentTypeSelectionInteractor.call(params: { document_type_selection_id: "root" })
       expect(result).to be_success
     end
+
+    it "fails if the document_type_selection_id isn't passed in" do
+      result = NewDocument::DocumentTypeSelectionInteractor.call(params: { document_type_selection_id: "" })
+      expect(result).to_not be_success
+    end
   end
 end
